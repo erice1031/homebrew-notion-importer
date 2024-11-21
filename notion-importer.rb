79696c9e571724a5f4b6c4f9ff8be165d1a23fec
Eric <eric.erwin@gmail.com>
2024-11-21 08:31:52 -0500

    Update notion-importer.rb

--- notion-importer.rb
+++ notion-importer.rb
@@ -2,7 +2,7 @@ class NotionImporter < Formula
   desc "CLI tool for importing property data into Notion"
   homepage "https://github.com/erice1031/homebrew-notion-importer"
   url "https://github.com/erice1031/homebrew-notion-importer/blob/main/notion-importer-1.0.0.tar.gz"
-  sha256 "5830a901068e60e1feaa85d08d5964c6c542cf955b1952cc9c5e477fc0ca6bbd"
+  sha256 "8ba659f9ac778999a2a7f263e9ee5e3fa7fd83a2dbe9cba62832c1c0cb44ce5e"
   license "MIT"
 
   depends_on "python@3.9"
